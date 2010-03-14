de6fe45cb972969f8321d3d3bcfb9680e2b19b99
Mike Boone <mike@boonedocks.net>
2010-03-13 21:56:49 -0500

    Included conditional indexing patch from https://wonsysos.16bugs.com/projects/2574/bugs/43703

--- lib/acts_as_fulltextable.rb
+++ lib/acts_as_fulltextable.rb
@@ -2,6 +2,11 @@
 #
 # 2008-03-07
 # Patched by Artūras Šlajus <x11@arturaz.net> for will_paginate support
+# 2008-06-21
+#   John Lane (www.starfry.com)
+#
+#   Added support for conditions to determine which records are included in the
+#   full text search. The condition is applied to a record on each update.
 require "fulltext_row"
 
 module ActsAsFulltextable
@@ -12,10 +17,13 @@ module ActsAsFulltextable
     # instance has changed before it actually updates the associated fulltext row.
     # If option :parent_id is not nulled, it is used as the field to be used as the parent of the record,
     # which is useful if you want to limit your queries to a scope.
+    # If option :conditions is given, it should be a string containing a ruby expression that 
+    # equates to true or nil/false. Records are tested with this condition and only those that return true
+    # add/update the FullTextRow. A record returning false that is already in FullTextRow is removed.
     #
     def acts_as_fulltextable(*attr_names)
-      configuration = { :check_for_changes => true, :parent_id => nil }
-      configuration.update(attr_names.pop) if attr_names.last.is_a?(Hash)
+      configuration = { :check_for_changes => true, :parent_id => nil, :conditions => "true" }
+      configuration.update(attr_names.pop) while attr_names.last.is_a?(Hash)
       configuration[:fields] = attr_names.flatten.uniq.compact
       write_inheritable_attribute 'fulltext_options', configuration
       extend  FulltextableClassMethods
@@ -61,7 +69,7 @@ module ActsAsFulltextable
     # Creates the fulltext_row record for self
     #
     def create_fulltext_record
-      FulltextRow.create(:fulltextable_type => self.class.to_s, :fulltextable_id => self.id, :value => self.fulltext_value, :parent_id => self.parent_id_value)
+      FulltextRow.create(:fulltextable_type => self.class.to_s, :fulltextable_id => self.id, :value => self.fulltext_value, :parent_id => self.parent_id_value) if eval self.class.fulltext_options[:conditions]
     end
     
     # Returns the parent_id value or nil if it wasn't set.
@@ -73,16 +81,21 @@ module ActsAsFulltextable
     # Updates self's fulltext_row record
     #
     def update_fulltext_record
-      if self.class.fulltext_options[:check_for_changes]
-        row = FulltextRow.find_by_fulltextable_type_and_fulltextable_id(self.class.to_s, self.id)
-        # It should never happen, but just in case we haven't got a row for the record, yet, create it instead of updating it.
-        if row.nil?
-          self.create_fulltext_record
-          return
+      if eval self.class.fulltext_options[:conditions]
+        if self.class.fulltext_options[:check_for_changes]
+          row = FulltextRow.find_by_fulltextable_type_and_fulltextable_id(self.class.to_s, self.id) 
+          # If we haven't got a row for the record, yet, create it instead of updating it.
+          if row.nil?
+            self.create_fulltext_record
+            return
+          end
         end
+        FulltextRow.update_all(["value = ?, parent_id = ?", self.fulltext_value, self.parent_id_value], ["fulltextable_type = ? AND fulltextable_id = ?", self.class.to_s, self.id]) if !(self.class.fulltext_options[:check_for_changes]) || (row.value != self.fulltext_value) || (self.parent_id_value != row.parent_id)
+      else
+        row = FulltextRow.find_by_fulltextable_type_and_fulltextable_id(self.class.to_s, self.id)
+        row.destroy unless row.nil?
       end
-      FulltextRow.update_all(["value = ?, parent_id = ?", self.fulltext_value, self.parent_id_value], ["fulltextable_type = ? AND fulltextable_id = ?", self.class.to_s, self.id]) if !(self.class.fulltext_options[:check_for_changes]) || (row.value != self.fulltext_value) || (self.parent_id_value != row.parent_id)
-    end
+    end  
     
     # Returns self's value created by concatenating fulltext fields for its class
     #
